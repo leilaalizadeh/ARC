3bdf0f93efce90d59551a786b09fac84954678b7
s329141@studenti.polito.it <s329141@studenti.polito.it>
2026-01-08 13:09:23 +0100

    2025

---
+++ 2025/20250212-1/Template/MY_CODE.s
@@ -0,0 +1,57 @@
+Y				RN 0
+N				RN 1
+CNT				RN 2
+TMP				RN 3
+TMP1			RN 4
+VAL				RN 5
+VAL1			RN 6
+RESULT			RN 7
+
+				AREA MY_CODE, CODE, READONLY
+
+Maclaurin		PROC
+				EXPORT Maclaurin
+				PUSH{R4-R8,R10-R11,LR}
+				
+				LDR TMP,=10
+				MUL VAL,TMP,Y
+				;ADD N,#1
+				LDR CNT,=1
+				MOV RESULT ,VAL
+				
+FOR 
+				CMP CNT,N
+				BHI FINISH
+				
+				MUL TMP,Y,Y
+;				NEG VAL,VAL
+				LDR TMP1,=0
+				SUB TMP1,#1
+				MUL VAL,TMP1
+				MUL TMP,VAL
+				
+				
+				LDR VAL1,=2
+				MUL VAL1,CNT
+				
+				ADD TMP1, VAL1,#1
+				
+				MUL TMP1,VAL1,TMP1
+				LDR VAL1,=100
+				MUL VAL1,TMP1
+				
+				SDIV VAL,TMP,VAL1
+				
+				ADD RESULT,VAL
+				
+				ADD CNT,#1
+				B FOR 				
+				
+FINISH
+				
+				MOV R0, RESULT	
+				
+				POP{R4-R8,R10-R11,PC}
+				ENDP
+			    END
+					
---
+++ 2025/20250212-1/Template/Timer/IRQ_timer.c
@@ -0,0 +1,70 @@
+/*********************************************************************************************************
+**--------------File Info---------------------------------------------------------------------------------
+** File name:           IRQ_timer.c
+** Last modified Date:  2014-09-25
+** Last Version:        V1.00
+** Descriptions:        functions to manage T0 and T1 interrupts
+** Correlated files:    timer.h
+**--------------------------------------------------------------------------------------------------------
+*********************************************************************************************************/
+#include "LPC17xx.h"
+#include "../Main.h"
+extern uint32_t tick;
+extern signed int Maclaurin(int y, int n);
+int sineValues[45];
+static int repeat = 0;
+static int ticks = 0;
+
+static int cast_to_int(float input){
+		if (input > 0)
+			input = input + 0.5f;
+		else
+			input = input - 0.5f;
+		return (int)input;
+}
+void TIMER0_IRQHandler (void)
+{
+  int input, output;
+	if (repeat < 200)
+	{
+		input =cast_to_int(1.428 * ticks); // see Note 1 below	
+		output = 500 + Maclaurin(input, 3) / 2;
+		sineValues[ticks + 22] = output; // see Note 2 below
+		DAC_write(output);
+		ticks ++;
+		if (ticks > 22)
+		{
+			ticks = -22;
+			repeat += 1;
+		}
+	}
+	else
+		DAC_write(0);
+	
+  LPC_TIM0->IR = 1;			/* clear interrupt flag */
+  return;
+}
+
+
+void TIMER1_IRQHandler (void)
+{
+  LPC_TIM1->IR = 1;			/* clear interrupt flag */
+  return;
+}
+
+void TIMER2_IRQHandler (void)
+{
+  LPC_TIM2->IR = 1;			/* clear interrupt flag */
+  return;
+}
+
+void TIMER3_IRQHandler (void)
+{
+	tick++;
+  LPC_TIM3->IR = 1;			/* clear interrupt flag */
+  return;
+}
+
+/******************************************************************************
+**                            End Of File
+******************************************************************************/
